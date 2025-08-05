df6499a3a043ea4edbea1b51742403598779e370
Dassjhwkwj <ddasun965@gmail.com>
2025-08-05 19:55:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || TFRQmAjS#MThp7mOb2rZB0cwZDvqz6n-NUOFa17AnV3mVOcE-6Uo, // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
